51ec75de9b56c7363ce81066eac51fc7151041a5
OleksiiHromovych <gromovuc@gmail.com>
2020-12-07 16:16:42 +0200

    Initial commit

---
+++ app/src/main/java/android/hromovych/com/tiras_test/MainActivity.kt
@@ -0,0 +1,11 @@
+package android.hromovych.com.tiras_test
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
