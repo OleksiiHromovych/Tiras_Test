45e7479afb4a28a6ff6061640f8c49c2a7250bdf
OleksiiHromovych <gromovuc@gmail.com>
2020-12-09 20:48:19 +0200

    Add start at reboot receiver

---
+++ app/src/main/java/android/hromovych/com/tiras_test/receivers/StartAtRebootReceiver.kt
@@ -0,0 +1,24 @@
+package android.hromovych.com.tiras_test.receivers
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.hromovych.com.tiras_test.MainActivity
+
+
+class StartAtRebootReceiver : BroadcastReceiver() {
+    /**If an app registers to receive broadcasts, the app's receiver consumes resources every time
+     * the broadcast is sent. This can cause problems if too many apps register to receive
+     * broadcasts based on system events; a system event that triggers a broadcast can cause all of
+     * those apps to consume resources in rapid succession, impairing the user experience. To
+     * mitigate this problem, Android 7.0 (API level 24) placed limitations on broadcasts, as
+     * described in Background Optimization. Android 8.0 (API level 26) makes these limitations
+     * more stringent.
+     * */
+    override fun onReceive(context: Context, intent: Intent) {
+        if (Intent.ACTION_BOOT_COMPLETED == intent.action) {
+            val serviceIntent = Intent(context, MainActivity::class.java)
+            context.startService(serviceIntent)
+        }
+    }
+}
